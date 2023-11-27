84a1ba01443128360379f74412a4042f60f69b74
Farhan <farhanahad3@gmail.com>
2023-11-27 23:53:09 +0600

    some file added

--- index.js
+++ index.js
@@ -61,6 +61,18 @@ async function run() {
       res.send({ token });
     });
 
+    // Warning: use verifyJWT before using verifyAdmin
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user?.role !== "admin") {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden message" });
+      }
+      next();
+    };
     //users related apis
 
     app.get("/users", async (req, res) => {
