c7c35ac98cb2e5253891d29720970a271001a073
Farhan <farhanahad3@gmail.com>
2023-11-28 12:12:00 +0600

    Add items & manage items added

--- index.js
+++ index.js
@@ -113,6 +113,18 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/menu", verifyJWT, verifyAdmin, async (req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+    app.delete("/menu/:id", verifyJWT, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
     //review related api's
     app.get("/reviews", async (req, res) => {
       const result = await reviewsCollection.find().toArray();
